74a4f379b30d9373fc27ed7e543bf39fedd509de
Viktor Kerkez <alefnula@gmail.com>
2013-07-27 22:41:28 +0200

    Small fixes

--- setup.py
+++ setup.py
@@ -2,40 +2,39 @@ __author__    = 'Viktor Kerkez <alefnula@gmail.com>'
 __date__      = '20 October 2010'
 __copyright__ = 'Copyright (c) 2010 Viktor Kerkez'
 
-import os
 import io
-import glob
 from distutils.core import setup
 
-setup(name='tea',
-      version='0.0.4',
-      description='Set of utility python modules.',
-      long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
-      platforms=['Windows', 'POSIX', 'MacOSX'],
-      author='Viktor Kerkez',
-      author_email='alefnula@gmail.com',
-      maintainer='Viktor Kerkez',
-      maintainer_email='alefnula@gmail.com',
-      url='https://bitbucket.org/alefnula/tea',
-      license='BSD',
-      packages=[
-          'tea',
-          'tea.commander',
-          'tea.console',
-          'tea.cron',
-          'tea.ds',
-          'tea.logger',
-          'tea.msg',
-          'tea.parsing',
-          'tea.process',
-          'tea.scm',
-          'tea.shutil',
-          'tea.system',
-          'tea.tests',
-          'tea.tornado',
-          'tea.utils',
-      ],
-      package_dir={'' : 'src'},
-      scripts=[
-      ],
+setup(
+    name='tea',
+    version='0.0.4',
+    description='Set of utility python modules.',
+    long_description=io.open('README.rst', 'r', encoding='utf-8').read(),
+    platforms=['Windows', 'POSIX', 'MacOSX'],
+    author='Viktor Kerkez',
+    author_email='alefnula@gmail.com',
+    maintainer='Viktor Kerkez',
+    maintainer_email='alefnula@gmail.com',
+    url='https://bitbucket.org/alefnula/tea',
+    license='BSD',
+    packages=[
+        'tea',
+        'tea.commander',
+        'tea.console',
+        'tea.cron',
+        'tea.ds',
+        'tea.logger',
+        'tea.msg',
+        'tea.parsing',
+        'tea.process',
+        'tea.scm',
+        'tea.shutil',
+        'tea.system',
+        'tea.tests',
+        'tea.tornado',
+        'tea.utils',
+    ],
+    package_dir={'' : 'src'},
+    scripts=[
+    ]
 )
