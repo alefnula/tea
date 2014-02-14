a9df53cadbde9722696910d4e3fe8d87837e766d
Viktor Kerkez <alefnula@gmail.com>
2014-02-14 15:21:00 +0100

    ENH: Config cleanup and adding __contains__

--- tea/ds/config.py
+++ tea/ds/config.py
@@ -11,20 +11,9 @@ import copy
 import logging
 import functools
 import threading
-from tea import shell
 from tea.utils import six
-try:
-    import yaml
-    has_yaml = True
-except ImportError:
-    has_yaml = False
-
 
 logger = logging.getLogger(__name__)
-
-
-class ConfigError(Exception):
-    pass
 
 
 def locked(func):
@@ -35,49 +24,29 @@ def locked(func):
     return wrapper
 
 
-def _get_format(filename):
-    return os.path.splitext(filename)[-1].lower()[1:]
-
-
-def _ensure_exists(filename, encoding='utf-8'):
-    """Ensures that the configuration file exists and that it produces a
-    correct empty configuration.
-    """
-    filename = os.path.abspath(filename)
-    if not os.path.isfile(filename):
-        dirname = os.path.dirname(filename)
-        if not os.path.isdir(dirname):
-            shell.mkdir(dirname)
-        with io.open(filename, 'w', encoding=encoding) as f:
-            fmt = _get_format(filename)
-            if fmt in (Config.JSON, Config.YAML):
-                f.write('{}')
-            f.write('\n')
-
-
 class Config(object):
     """Configuration class"""
     DICT = 'dict'
     JSON = 'json'
     YAML = 'yaml'
 
-    ensure_exists = staticmethod(_ensure_exists)
-
     def __init__(self, filename=None, data=None, fmt=None, encoding='utf-8',
-                 autosave=True):
+                 auto_save=True):
         self.lock = threading.Lock()
         self.encoding = encoding
-        self.autosave = autosave
+        self.auto_save = auto_save
         if filename is not None:
             self.filename = os.path.abspath(filename)
-            self.format = (fmt if fmt is not None
-                           else _get_format(self.filename))
+            self.fmt = (
+                fmt if fmt is not None
+                else os.path.splitext(self.filename)[-1].lower()[1:]
+            )
             self.data = self._read_file()
         else:
             self.filename = filename
-            self.format = Config.JSON if fmt is None else fmt
+            self.fmt = Config.JSON if fmt is None else fmt
             if isinstance(data, dict):
-                self.format = Config.DICT
+                self.fmt = Config.DICT
                 self.data = copy.deepcopy(data)
             elif isinstance(data, six.string_types):
                 self.data = self._read_string(data)
@@ -90,61 +59,48 @@ class Config(object):
                            self.filename)
             return {}
         try:
-            if self.format == Config.JSON:
+            if self.fmt == Config.JSON:
                 with io.open(self.filename, 'r+b') as f:
                     return json.load(f, encoding=self.encoding)
-            elif self.format == Config.YAML:
-                if has_yaml:
-                    with io.open(self.filename, 'r',
-                                 encoding=self.encoding) as f:
-                        return yaml.safe_load(f)
-                else:
-                    logger.error('YAML is not installed.')
-                    return {}
+            elif self.fmt == Config.YAML:
+                import yaml
+                with io.open(self.filename, 'r', encoding=self.encoding) as f:
+                    return yaml.safe_load(f)
             else:
-                logger.error('Unsupported configuration format: %s',
-                             self.format)
+                logger.error('Unsupported configuration format: %s', self.fmt)
                 return {}
         except Exception as e:
             logger.error('Failed to load file "%s" in format "%s". %s',
-                         self.filename, self.format, e)
+                         self.filename, self.fmt, e)
             return {}
 
     def _read_string(self, data):
         try:
             if isinstance(data, bytes):
                 data = data.decode(self.encoding)
-            if self.format == Config.JSON:
+            if self.fmt == Config.JSON:
                 return json.loads(data)
-            elif self.format == Config.YAML:
-                if has_yaml:
-                    return yaml.safe_load(data)
-                else:
-                    logger.error('YAML is not installed.')
-                    return {}
+            elif self.fmt == Config.YAML:
+                import yaml
+                return yaml.safe_load(data)
             else:
-                logger.error('Unsupported configuration format: %s',
-                             self.format)
+                logger.error('Unsupported configuration format: %s', self.fmt)
                 return {}
-        except:
-            logger.exception('Failed to load data in format "%s"', self.format)
+        except Exception as e:
+            logger.error('Failed to load data in format "%s". %s', self.fmt, e)
             return {}
 
     def save(self):
         if self.filename is not None:
-            if self.format == Config.JSON:
+            if self.fmt == Config.JSON:
                 with io.open(self.filename, 'w+b') as f:
                     json.dump(self.data, f, indent=2, encoding=self.encoding)
-            elif self.format == Config.YAML:
-                if has_yaml:
-                    with io.open(self.filename, 'w',
-                                 encoding=self.encoding) as f:
-                        yaml.safe_dump(self.data, f, default_flow_style=False)
-                else:
-                    logger.error('YAML is not installed,')
+            elif self.fmt == Config.YAML:
+                import yaml
+                with io.open(self.filename, 'w', encoding=self.encoding) as f:
+                    yaml.safe_dump(self.data, f, default_flow_style=False)
             else:
-                logger.error('Unsupported configuration format: %s',
-                             self.format)
+                logger.error('Unsupported configuration format: %s', self.fmt)
 
     def __get(self, var, create=False):
         current = self.data
@@ -178,7 +134,7 @@ class Config(object):
         elif isinstance(current, list):
             item = int(item, 10)
             current[item] = value
-        if self.autosave:
+        if self.auto_save:
             self.save()
 
     def __del(self, var):
@@ -193,7 +149,7 @@ class Config(object):
         elif isinstance(current, list):
             item = int(item, 10)
             current.pop(item)
-        if self.autosave:
+        if self.auto_save:
             self.save()
 
     @locked
@@ -214,6 +170,14 @@ class Config(object):
     def __delitem__(self, item):
         """Unsafe version, may raise KeyError or IndexError"""
         return self.__del(item)
+
+    @locked
+    def __contains__(self, item):
+        try:
+            self.__get(item)
+            return True
+        except (KeyError, IndexError):
+            return False
 
     @locked
     def get(self, var, default=None):
@@ -247,30 +211,28 @@ class Config(object):
             current.append(value)
         else:
             current.insert(index, value)
-        if self.autosave:
+        if self.auto_save:
             self.save()
 
     def __repr__(self):
-        return ('Config(filename="%(filename)s", format="%(format)s", '
-                'encoding="%(encoding)s", autosave=%(autosave)s)' %
+        return ('Config(filename="%(filename)s", format="%(fmt)s", '
+                'encoding="%(encoding)s", auto_save=%(auto_save)s)' %
                 self.__dict__)
 
 
 class MultiConfig(object):
     """Base class for configuration management"""
 
-    ensure_exists = staticmethod(_ensure_exists)
-
     def __init__(self, filename=None, data=None, fmt=None, encoding='utf-8',
-                 autosave=True):
+                 auto_save=True):
         self.lock = threading.Lock()
         self.__configs = []
-        self.attach(filename, data, fmt, encoding, autosave)
+        self.attach(filename, data, fmt, encoding, auto_save)
 
     @locked
     def attach(self, filename=None, data=None, fmt=None, encoding='utf-8',
-               autosave=True, index=None):
-        config = Config(filename, data, fmt, encoding, autosave)
+               auto_save=True, index=None):
+        config = Config(filename, data, fmt, encoding, auto_save)
         if index is None:
             self.__configs.insert(0, config)
         else:
@@ -332,9 +294,17 @@ class MultiConfig(object):
         return self.__set(item, value)
 
     @locked
-    def __delitem(self, item):
+    def __delitem__(self, item):
         """Unsafe version, may raise KeyError or IndexError"""
         self.__del(item)
+
+    @locked
+    def __contains__(self, item):
+        try:
+            self.__get(item)
+            return True
+        except (KeyError, IndexError):
+            return False
 
     @locked
     def get(self, var, default=None):
