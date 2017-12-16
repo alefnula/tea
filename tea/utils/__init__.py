aec17f76513713c7656471cfbfca5eb7169255e1
Viktor Kerkez <alefnula@gmail.com>
2017-12-16 12:49:02 +0100

    Added star query to the get_object function.

--- tea/utils/__init__.py
+++ tea/utils/__init__.py
@@ -16,7 +16,13 @@ logger = logging.getLogger(__name__)
 
 def _package(module):
     """This is a hack for python2 since it does not have __package__ always set
-    up correctly"""
+    up correctly
+
+    Args:
+        module: Module for which we want the package name
+    Returns:
+        str: Model's package name
+    """
     return (
         module.__name__ if module.__package__ is None else module.__package__
     )
@@ -25,10 +31,19 @@ def _package(module):
 def get_object(path='', obj=None):
     """Returns an object from a dot path.
 
-    Path can either be a full path, in which case the `get_object`
-    function will try to import the module and follow the path. Or
-    it can be a path relative to the object passed in as the second
+    Path can either be a full path, in which case the `get_object` function
+    will try to import the modules in the path and follow it to the final
+    object. Or it can be a path relative to the object passed in as the second
     argument.
+
+    Args:
+        path (str): Full or relative dot path to the desired object
+        obj (object): Starting object. Dot path is calculated relatively to
+            this object.
+
+    Returns:
+        object: Object at the end of the path, or list of non hidden objects
+            if we use the star query.
 
     Example for full paths::
 
@@ -42,6 +57,18 @@ def get_object(path='', obj=None):
         >>> import os
         >>> get_object('path.join', os)
         <function join at 0x1002d9ed8>
+
+    Example for a star query. (Star query can be used only as the last element
+    of the path::
+
+        >>> get_object('tea.dsa.*')
+        []
+        >>> get_object('tea.dsa.singleton.*')
+        [<class 'tea.dsa.singleton.Singleton'>,
+         <class 'tea.dsa.singleton.SingletonMetaclass'>
+         <module 'six' from '...'>]
+        >>> get_object('tea.dsa.*')
+        [<module 'tea.dsa.singleton' from '...'>]    # Since we imported it
     """
     if not path:
         return obj
@@ -50,6 +77,12 @@ def get_object(path='', obj=None):
         obj = importlib.import_module(path[0])
         path = path[1:]
     for item in path:
+        if item == '*':
+            # This is the star query, returns non hidden objects
+            return [
+                getattr(obj, name) for name in dir(obj)
+                if not name.startswith('__')
+            ]
         if isinstance(obj, types.ModuleType):
             submodule = '{}.{}'.format(_package(obj), item)
             try:
@@ -121,11 +154,11 @@ def load_subclasses(klass, modules=None):
     """Load recursively all submodules of the modules and return all the
     subclasses of the provided class
 
-    :param klass: Class whose subclasses we want to load.
-    :type modules: str or list[str]
-    :param modules: List of additional modules or module names that should be
-        recursively imported in order to find all the subclasses of the
-        desired class. Default: None
+    Args:
+        klass (str or list of str): Class whose subclasses we want to load.
+        modules: List of additional modules or module names that should be
+            recursively imported in order to find all the subclasses of the
+            desired class. Default: None
 
     FIXME: This function is kept only for backward compatibility reasons, it
         should not be used. Deprecation warning should be raised and it should
